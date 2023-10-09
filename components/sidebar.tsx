ec2c2ec19ee53d8784369c974a9c765721b52c00
Timothy Molina <60799704+Superboy20@users.noreply.github.com>
2023-10-09 12:03:34 -0400

    added an API limit counter in the UI so the user can see specific info in regards to their status as a free-tier user.

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { LayoutDashboard, MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
+import { FreeCounter } from "@/components/free-counter";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -54,7 +55,13 @@ const routes = [
     }
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+    apiLimitCount: number;
+};
+
+const Sidebar = ({
+    apiLimitCount = 0
+}: SidebarProps) => {
     const pathname = usePathname();
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
@@ -79,6 +86,9 @@ const Sidebar = () => {
                 </div>
 
             </div>
+            <FreeCounter
+                apiLimitCount={apiLimitCount}
+            />
         </div>
     );
 }
