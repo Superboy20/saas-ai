88bb29dbc07c4fb9585196eddbc62ebf151a7f78
Timothy Molina <60799704+Superboy20@users.noreply.github.com>
2023-10-10 18:22:54 -0400

    fixed styling issues and added placeholder names for testimonials

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -8,7 +8,7 @@ import { Button } from "@/components/ui/button";
 export const LandingHero = () => {
     const { isSignedIn } = useAuth();
     return (
-        <div className="text-white font-bold py-6 text-center space-y-5">
+        <div className="text-white font-bold py-36 text-center space-y-5">
             <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
                 <h1>A Great All-In-One AI Tool for</h1>
                 <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-amber-500">
