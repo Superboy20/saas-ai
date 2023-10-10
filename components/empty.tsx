45740a9f875229aa39af36003844e8370802da4b
Timothy Molina <60799704+Superboy20@users.noreply.github.com>
2023-10-10 17:32:55 -0400

    fixed lint error, false positive

--- components/empty.tsx
+++ components/empty.tsx
@@ -2,7 +2,7 @@ export const Empty = () => {
     return (
         <div className="h-full p-10 flex flex-col items-center justify-center">
             <div className="relative h-72 w-92">
-                <h1 className="text-lg font-medium text-black text-opacity-50">There's nothing here, ask me something!</h1>
+                <h1 className="text-lg font-medium text-black text-opacity-50">Hmm... This page looks empty, ask me something!</h1>
             </div>
         </div>
     )
